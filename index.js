91030715771cd73eb7af17b57cec9286b6ed99e1
ninjawarrior1337 <t.rothled@gmail.com>
2016-10-29 20:27:32 -0700

    21

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var person = prompt("Please enter your religion", "Teelarism");
 
-document.write("This religion name is " + person.length + "letters long")
+document.write("This religion name is " + person.length + " letters long")
