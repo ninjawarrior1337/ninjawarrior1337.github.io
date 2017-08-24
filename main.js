609dc4d9b35ad983934ec6748f170778bdd122e7
ninjawarrior1337 <ninjawarrior1337@users.noreply.github.com>
2017-08-24 09:53:51 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-var bg = document.getElementByClassName("a")
-
-cheet('up up down down left right left right b a', function ()
-{
-
-}
-)
-;
+cheet('up up down down left right left right b a', function () { 
+    var vertdnag = document.getElementsByClassName("lelsame")
+    
+    if (vertdnag.item(0).getAttribute("id") == "a")
+    {
+        vertdnag.item(0).setAttribute("id", "rainbow")
+    }
+    else
+    {
+        vertdnag.item(0).setAttribute("id", "a")
+    }
+    
+    
+});
