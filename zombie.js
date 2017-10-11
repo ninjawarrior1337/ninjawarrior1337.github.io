931bec29a3065395faf3883b544331ff18a38038
ninjawarrior1337 <t.rothled@gmail.com>
2017-10-10 21:07:11 -0700

    Finished js project1

--- zombie.js
+++ zombie.js
@@ -1,14 +1,10 @@
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
-    }
-  }
-}
+numInequalities = 99999999999
+var gameTimer;
 
-for (var i = 0; i < 10; i++)
+function winGame()
 {
     document.getElementById("btnToggleInequality").click();
     document.getElementById("btnFireInequality").click();
 }
+
+gameTimer = setInterval(winGame, 500)
