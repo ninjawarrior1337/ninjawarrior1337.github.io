e37eda9fbff6167f8b6a511fb27f210bb6967feb
unknown <118882@ecrchs.org>
2017-10-19 09:53:05 -0700

    Added 2 new js projects

---
+++ ecr/fall17/per2/monkeyshooter/engine.js
@@ -0,0 +1,43 @@
+var monkey0;
+var gameTime;
+var output;
+var hitVal = 0;
+
+function init(){
+    monkey0 = document.getElementById("monkey0");
+    output = document.getElementById("output");
+    
+    gameTime = setInterval(function(){
+        loop();
+    }, 10)
+
+    output.innerHTML = "Score: " + hitVal;
+
+    reloacate();
+}
+
+function reloacate(){
+    var x = Math.floor(Math.random()*421)
+    monkey0.style.left = x + "px";
+    monkey0.style.top = "260px";
+    y = 400;
+}
+
+function hitMonkey()
+{
+    hitVal++;
+    output.innerHTML = "Score: " + hitVal;
+    reloacate();
+}
+
+function loop(){
+    var y = parseInt(monkey0.style.top) - 5;
+    if ( y < -100 )
+        {
+            reloacate();
+        }
+        else
+        {
+            monkey0.style.top = y + "px";                
+        }
+}
