5a94ba304bc3d9366a7e74371ca4688d169129d3
unknown <118882@ecrchs.org>
2017-10-25 10:49:40 -0700

    Added more features to breaking bad

--- ecr/fall17/per2/breakingBad/main.js
+++ ecr/fall17/per2/breakingBad/main.js
@@ -1,8 +1,17 @@
+function getEId(str)
+{
+    return document.getElementById(str)
+}
+
 var title = document.getElementById("title");
 
 var imgActor = document.getElementById("imgActor");
 
 var content = document.getElementById("content");
+
+var jb = document.getElementById("jesse");
+var hb = getEId("hank");
+var wb = getEId("walt");
 
 var stats = document.getElementById("stats");
 var kills = document.getElementById("kills");
@@ -22,7 +31,44 @@ function updateStats(name)
     {
         kills.innerHTML = "Kills: 10";
         hotness.innerHTML = "Hotness: 8.9";
-        worth.innerHTML = "Worth: $10B"
+        worth.innerHTML = "Worth: $1";
+    }
+
+    if (name == 'hank')
+    {
+        kills.innerHTML = "Kills: 4";
+        hotness.innerHTML = "Hotness: 3.0";
+        worth.innerHTML = "Worth: $10K"
+    }
+}
+
+function changeColor(name)
+{
+    if(name == 'walt')
+    {
+        wb.style.color = "blue"
+        jb.style.color = null;
+        hb.style.color = null;
+        
+        title.style.color = "white";
+    }
+
+    if(name == 'hank')
+    {
+        wb.style.color = null;
+        jb.style.color = null;
+        hb.style.color = "blue";
+
+        title.style.color = "black";        
+    }
+
+    if(name == 'jesse')
+    {
+        wb.style.color = null;
+        jb.style.color = "blue";
+        hb.style.color = null;
+
+        title.style.color = "white";        
     }
 }
 
@@ -34,6 +80,7 @@ function updateInfo(name)
         imgActor.src = "walt.jpg";
         content.style.backgroundColor = "navy";
         stats.style.color = "aqua";
+        changeColor(name);
         updateStats(name);
     }
     
@@ -43,6 +90,17 @@ function updateInfo(name)
         imgActor.src = "jesse.jpg";
         content.style.backgroundColor = "pink";
         stats.style.color = "navy";
+        changeColor(name);
+        updateStats(name);
+    }
+
+    if (name == 'hank')
+    {
+        title.innerHTML = "Hank";
+        imgActor.src = "hank.jpg";
+        content.style.backgroundColor = "lavender";
+        stats.style.color = "navy";
+        changeColor(name);
         updateStats(name);
     }
 }
